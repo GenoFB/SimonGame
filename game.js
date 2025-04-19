48e3bbf5db7385e91895beb0596462fe2acdf3bf
geno <genaro.flores.bustos1@gmail.com>
2025-04-19 12:29:13 -0400

    game finished

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
-let buttonColours = ["red", "blue", "green", "yellow"];
 let gameStarted = false;
 let keyCount = 0;
 let level = 0;
@@ -12,12 +12,12 @@ $(document).on("keydown", function (e) {
     if (gameStarted && keyCount === 1) {
         $("#level-title").text("Level " + level);
         nextSequence();
-
     }
 });
 
 
 function nextSequence() {
+    userClickedPattern = [];
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
@@ -30,11 +30,13 @@ function nextSequence() {
 
 }
 
+//User Click
 $(".btn").on("click", function () {
     let userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
 })
 
 function playSound(name) {
@@ -47,4 +49,37 @@ function animatePress(currentColour) {
     setTimeout(() => {
         $("#" + currentColour).removeClass("pressed");
     }, 100);
+}
+
+function startOver() {
+    level = 0;
+    keyCount = 0;
+    gameStarted = false;
+    gamePattern = [];
+
+}
+
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        console.log("success")
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        let wrongSound = new Audio("./sounds/wrong.mp3");
+        wrongSound.play();
+
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 300);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        console.log("wrong")
+        startOver();
+        
+    }
 }
