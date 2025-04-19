8bbe1b1f414212940192828a519832ceca6879f2
geno <genaro.flores.bustos1@gmail.com>
2025-04-18 21:10:05 -0400

    first sound and animation added

--- game.js
+++ game.js
@@ -3,6 +3,11 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 let randomChosenColour = buttonColours[nextSequence()];
 gamePattern.push(randomChosenColour);
 
+//Animation & sound
+$("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+let audio = new Audio("./sounds/" + randomChosenColour + ".mp3")
+audio.play();
+
 //
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4 );
